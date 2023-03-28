b823215dadecfef2c9097524c411ba08ee87efc0
divyakondakrindi <118252095+divyakondakrindi@users.noreply.github.com>
2023-03-28 18:14:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,29 @@
-function fn(){
-    var Name = document.getElementById('name').value;
-  var year = document.getElementById('year').value;
-    document.getElementById('url').innerHTML += '<br>'+"https://localhost:8080/"+"?Name="   +Name+"&"+"?year="+year ;
+// your code here
+let heading = document.querySelector('#url');
+let nameElement = document.querySelector('#name');
+let yearElement = document.querySelector('#year');
+let submit = document.querySelector('#button')
+
+
+
+function changeUrl(){
+    let headingText = "https://localhost:8080/";
+    let nameValue = nameElement.value;
+    let yearValue = yearElement.value;
+    console.log(heading , nameElement ,yearElement , submit, nameValue, yearValue)
+    if (!nameValue == "" && !yearValue == "") {
+        headingText += '?name=' + nameValue + '&year=' + yearValue;
+    }
+    else if(nameValue == "" && !yearValue == ""){
+        headingText += '?year=' + yearValue;
+    }
+    else if(yearValue=="" && nameValue!=""){
+        headingText += '?name=' + nameValue;
+    }
+    else{
+        headingText = headingText;
+    }
+    heading.textContent = headingText
 }
+
+submit.addEventListener('click' , changeUrl)
