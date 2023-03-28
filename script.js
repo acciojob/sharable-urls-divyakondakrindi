d62e48355271f1d2cb76a9506e8fc07183610903
divyakondakrindi <118252095+divyakondakrindi@users.noreply.github.com>
2023-03-28 17:33:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function fn(){
     var Name = document.getElementById('name').value;
   var year = document.getElementById('year').value;
-    document.getElementById('url').innerHTML += '<br>'+"https://localhost:8080/"+"Name="   +Name+"&"+"Year="+year ;
+    document.getElementById('url').innerHTML += '<br>'+"https://localhost:8080/"+""   +Name+"&"+""+year ;
 }
